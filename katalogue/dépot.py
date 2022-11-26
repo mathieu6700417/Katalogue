b9013f584c88fd10e9fc514db23adc8dea402ffc
Laurent <laurentpeyronnet@free.fr>
2022-11-26 16:35:45 +0100

    Ajout dépôt association avec gestion archive et sauvegarde locale

--- katalogue/dépot.py
+++ katalogue/dépot.py
@@ -1,6 +1,9 @@
 import os
 import yaml
 import requests
+import zipfile
+from pathlib import Path
+import pandas as pd
 
 class Dépot:
     def __init__(self, nom_fournisseur):
@@ -24,14 +27,48 @@ class Dépot:
         
     def télécharger(self, nom_source, forcer_téléchargement=False):
         url_fichier = self.config.get(nom_source).get('url')
-        nom_fichier = self.config.get(nom_source).get('fichier')
-        
-        chemin_fichier = os.path.join(self.dossier, nom_fichier)
+        chemin_fichier = self.chemin_fichier(nom_source)
+
         if not(os.path.exists(chemin_fichier)) or forcer_téléchargement:
             resp = requests.get(url_fichier)
             with open(chemin_fichier, "wb") as f:
                 f.write(resp.content)
+
+    def désarchiver(self, nom_source, ecraser_cible=False):
+        url_fichier = self.config.get(nom_source).get('url')
+        nom_fichier = self.config.get(nom_source).get('fichier')
+        chemin_archive = os.path.join(self.dossier, nom_fichier)
+        chemin_extraction = os.path.join(self.dossier, nom_source)
         
+        if not(os.path.exists(chemin_extraction)) or ecraser_cible:
+            with zipfile.ZipFile(chemin_archive, 'r') as zip_ref:
+                Path(chemin_extraction).mkdir(parents=True, exist_ok=True)
+                zip_ref.extractall(chemin_extraction)
         
     def chemin_fichier(self, nom_source):
         return os.path.join(self.dossier, self.config.get(nom_source).get('fichier'))
+
+    def chemin_extraction(self, nom_source):
+        return os.path.join(self.dossier, nom_source)
+
+    def gérer_sauvegarde_locale(fonction):
+        def fonction_avec_sauvegarde(param, sauvegarder_localement): #TODO: gestion générique des params cas autres
+            nom_dépôt = param.__class__.__name__
+            nom_fonction = fonction.__name__
+            chemin_sauvegarde_locale = os.path.join(param.dossier,f'locale_{nom_dépôt}_{nom_fonction}.pkl')
+
+            if sauvegarder_localement:
+                if not(os.path.exists(chemin_sauvegarde_locale)): 
+                    print("Aucune sauvegarde existante, calcul")
+                    df = fonction(param)
+                    df.to_pickle(chemin_sauvegarde_locale) 
+                    print("Sauvegarde locale réalisée ", chemin_sauvegarde_locale)
+                else:
+                    print("Lecture sauvegarde existante ", chemin_sauvegarde_locale)
+                    df = pd.read_pickle(chemin_sauvegarde_locale)
+            else:
+                df = fonction(param)
+
+            return df
+
+        return fonction_avec_sauvegarde
