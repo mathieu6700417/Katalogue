e2a8f2454524668b074fd8735c12008fc401e9e5
Laurent <laurentpeyronnet@free.fr>
2022-11-26 17:44:18 +0100

    Gestion paramêtre générique

--- katalogue/dépot.py
+++ katalogue/dépot.py
@@ -52,22 +52,22 @@ class Dépot:
         return os.path.join(self.dossier, nom_source)
 
     def gérer_sauvegarde_locale(fonction):
-        def fonction_avec_sauvegarde(param, sauvegarder_localement): #TODO: gestion générique des params cas autres
-            nom_dépôt = param.__class__.__name__
+        def fonction_avec_sauvegarde(self, *argparams, **kwparams): #param, sauvegarder_localement): #TODO: gestion générique des params cas autres
+            nom_dépôt = self.__class__.__name__
             nom_fonction = fonction.__name__
-            chemin_sauvegarde_locale = os.path.join(param.dossier,f'locale_{nom_dépôt}_{nom_fonction}.pkl')
-
-            if sauvegarder_localement:
+            chemin_sauvegarde_locale = os.path.join(self.dossier,f'locale_{nom_dépôt}_{nom_fonction}.pkl')
+            
+            if kwparams['sauvegarder_localement']:
                 if not(os.path.exists(chemin_sauvegarde_locale)): 
                     print("Aucune sauvegarde existante, calcul")
-                    df = fonction(param)
+                    df = fonction(self, *argparams, **kwparams)
                     df.to_pickle(chemin_sauvegarde_locale) 
                     print("Sauvegarde locale réalisée ", chemin_sauvegarde_locale)
                 else:
                     print("Lecture sauvegarde existante ", chemin_sauvegarde_locale)
                     df = pd.read_pickle(chemin_sauvegarde_locale)
             else:
-                df = fonction(param)
+                df = fonction(self, *argparams, **kwparams)
 
             return df
 
